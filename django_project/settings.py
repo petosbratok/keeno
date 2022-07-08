e6b30645bf5a5689cd85b7f02bc38797e5c5ae9e
petosbratok <44753312+petosbratok@users.noreply.github.com>
2022-07-09 00:11:06 +0300

    make SQLite a default DB

--- django_project/settings.py
+++ django_project/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 #
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'keeno',
-        'USER': 'postgres',
-        'PASSWORD': os.environ.get('default_pass'),
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'keeno',
+#         'USER': 'postgres',
+#         'PASSWORD': os.environ.get('default_pass'),
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
